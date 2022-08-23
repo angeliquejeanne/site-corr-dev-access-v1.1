2774650fee3a0a947fff308a7014f2a9ac27ce86
AngeliqueJeanne <60384148+angeliquejeanne@users.noreply.github.com>
2022-08-23 11:04:57 +0200

    Add files via upload

--- src/script/Increment-Decrement.js
+++ src/script/Increment-Decrement.js
@@ -1,1 +1,13 @@
+let total = 0;
+
+    $(document).ready(function() {  //Pour décocher les cases à l'actualisation de la page
+        console.log('ready!'); 
+        $('.checkbox').prop('checked', true);
+        $('.checkbox').prop('checked', false);
+
+        $('.checkbox').on('change', function() { //Incrémentation/Désincrémentation du compteur quand case cocher ou  décocher
+            $('.total').html(total += this.checked ? 1 : -1);
+        });
+    });
+
 
